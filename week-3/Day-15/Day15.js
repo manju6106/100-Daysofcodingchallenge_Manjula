b9e71e17b91671d91760eabc52735c16daaae49e
Manjula Rachamreddy <130343542+manju6106@users.noreply.github.com>
2024-02-20 23:33:54 +0530

    Add files via upload

---
+++ week-3/Day-15/Day15.js
@@ -0,0 +1,25 @@
+console.log("Hello, world!"); 
+
+// Assignment
+let c = 10;
+console.log(c); // Output: 10
+
+// Addition assignment
+c += 5; // c is now 15
+console.log(c); // Output: 15
+
+// Subtraction assignment
+c -= 3; // c is now 12
+console.log(c); // Output: 12
+
+// Multiplication assignment
+c *= 2; // c is now 24
+console.log(c); // Output: 24
+
+// Division assignment
+c /= 4; // c is now 6
+console.log(c); // Output: 6
+
+// Remainder assignment
+c %= 4; // c is now 2
+console.log(c); // Output: 2
