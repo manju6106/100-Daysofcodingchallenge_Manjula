66507e0988395302018f4eeda44a6ae14eb451dd
Manjula Rachamreddy <130343542+manju6106@users.noreply.github.com>
2024-03-06 22:17:36 +0530

    Add files via upload

---
+++ week-5/Day-30/Day30.py
@@ -0,0 +1,15 @@
+# Example with arbitrary values
+first_name = "Manjula"
+greeting_message = "Hello, " + first_name + "!"
+print(greeting_message)
+
+# Assuming an arbitrary age
+age = 20
+square_of_age = age ** 2
+print("Square of age:", square_of_age)
+
+# Assuming an arbitrary height
+height = 5.2 # in meters
+weight = 54 # in kilograms
+bmi = weight / (height ** 2)
+print("BMI:", bmi)
